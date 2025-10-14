0e5aa63e6312a0a5a43c3b2b5acff322d376106c
7ogeid <diegofabrocandido77@gmail.com>
2025-10-14 20:59:22 -0300

    feat: Add theme-settings component

---
+++ src/app/components/profile/theme-settings/theme-settings.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-settings',
+  imports: [],
+  templateUrl: './theme-settings.html',
+  styleUrl: './theme-settings.scss'
+})
+export class ThemeSettingsComponent {
+
+}
